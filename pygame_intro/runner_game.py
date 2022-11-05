a01eafcc5647d2ebabf15cea7d875c205337eb1a
Jason Cameron <dev.jasoncameron@gmail.com>
2022-11-04 20:27:47 -0500

    add text with font

--- pygame_intro/runner_game.py
+++ pygame_intro/runner_game.py
@@ -8,9 +8,13 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 
+# TXT
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
 # SUR
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('My game', False, 'Black')
 
 
 # RUN
@@ -22,7 +26,8 @@ while True:
 
         # Draw all our elements
         screen.blit(sky_surface, (0, 0))
-        screen.blit(ground_surface, (0, 240))
+        screen.blit(ground_surface, (0, 300))
+        screen.blit(text_surface, (300, 50))
 
         # Update everything
 
