f8edcf7daf06eab4fc34ec746947f62dcdc39aea
Jason Cameron <dev.jasoncameron@gmail.com>
2022-11-16 21:42:01 -0600

    anim comp with player obj

--- pygame_intro/runner_game.py
+++ pygame_intro/runner_game.py
@@ -34,9 +34,19 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom >= 300:
             self.rect.bottom = 300
 
+    def animation_state(self):
+        if self.rect.bottom < 300:
+            self.image = self.player_jump
+        else:
+            self.player_index += 0.1
+            if self.player_index >= len(self.player_walk):
+                self.player_index = 0
+            self.image = self.player_walk[int(self.player_index)]
+
     def update(self):
         self.player_input()
         self.apply_gravity()
+        self.animation_state()
 
 
 def display_score():
