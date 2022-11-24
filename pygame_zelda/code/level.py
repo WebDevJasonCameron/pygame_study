9569099e9722445577196c1397e0439704c69ec7
Jason Cameron <dev.jasoncameron@gmail.com>
2022-11-24 09:51:25 -0600

    add camera

--- pygame_zelda/code/level.py
+++ pygame_zelda/code/level.py
@@ -31,9 +31,8 @@ class Level:
 
     def run(self):
         # update and draw the game
-        self.visible_sprites.custom_draw()
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
@@ -42,7 +41,16 @@ class YSortCameraGroup(pygame.sprite.Group):
         # gen setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2(100, 200)
 
-    def custom_draw(self):
+    def custom_draw(self, player):
+
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
         for sprite in self.sprites():
-            self.display_surface.blit(sprite.image, sprite.rect)
+            offset_pos = sprite.rect.topleft + self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
