da5ee23a79f68dfe3387af746d12e0e1e01e99f3
Jason Cameron <dev.jasoncameron@gmail.com>
2022-11-23 20:34:37 -0600

    comp first image testing of a map

--- pygame_zelda/code/level.py
+++ pygame_zelda/code/level.py
@@ -1,4 +1,7 @@
 import pygame
+from settings import *
+from tile import Tile
+from player import Player
 
 
 class Level:
@@ -8,9 +11,22 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # sprite group setup
-        self.visble_sprites = pygame.sprite.Group()
+        self.visible_sprites = pygame.sprite.Group()
         self.obstacles = pygame.sprite.Group()
+
+        # sprite setup
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites])
+                if col == 'p':
+                    Player((x, y), [self.visible_sprites])
 
     def run(self):
         # update and draw the game
-        pass
+        self.visible_sprites.draw(self.display_surface)
