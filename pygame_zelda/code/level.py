0d348e254f9e2aef354a42e8a3f84c785e15156b
Jason Cameron <dev.jasoncameron@gmail.com>
2022-11-24 10:56:45 -0600

    adding a floor

--- pygame_zelda/code/level.py
+++ pygame_zelda/code/level.py
@@ -45,13 +45,22 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2(100, 200)
 
+        # creating the floor
+        self.floor_surf = pygame.image.load(
+            '../graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
+
     def custom_draw(self, player):
 
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        # for sprite in self.sprites():
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
+
+        # set sprite offset
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
