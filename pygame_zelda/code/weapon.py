dea6604068f630f97b53e67450909e05c19ee7fc
Jason Cameron <dev.jasoncameron@gmail.com>
2022-11-27 09:53:48 -0600

    pos attack by player dir

--- pygame_zelda/code/weapon.py
+++ pygame_zelda/code/weapon.py
@@ -3,6 +3,16 @@ import pygame
 
 class Weapon(pygame.sprite.Sprite):
     def __init__(self, player, groups):
-        super().__init__()
+        super().__init__(groups)
+        direction = player.status.split('_')[0]
+        print(direction)
+
+        # graphic
         self.image = pygame.Surface((40, 40))
         self.rect = self.image.get_rect(center=player.rect.center)
+
+        # placement
+        if direction == 'right':
+            self.rect = self.image.get_rect(midleft=player.rect.midright)
+        else:
+            self.rect = self.image.get_rect(center=player.rect.center)
