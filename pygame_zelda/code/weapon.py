78f709d682f442b5e61b87187cac176013918ece
Jason Cameron <dev.jasoncameron@gmail.com>
2022-11-28 20:01:26 -0600

    add wpn directions

--- pygame_zelda/code/weapon.py
+++ pygame_zelda/code/weapon.py
@@ -13,5 +13,12 @@ class Weapon(pygame.sprite.Sprite):
         if direction == 'right':
             self.rect = self.image.get_rect(
                 midleft=player.rect.midright + pygame.math.Vector2(0, 16))
+        elif direction == 'left':
+            self.rect = self.image.get_rect(
+                midright=player.rect.midleft + pygame.math.Vector2(0, 16))
+        elif direction == 'down':
+            self.rect = self.image.get_rect(
+                midtop=player.rect.midbottom + pygame.math.Vector2(-10, 0))
         else:
-            self.rect = self.image.get_rect(center=player.rect.center)
+            self.rect = self.image.get_rect(
+                midbottom=player.rect.midtop + pygame.math.Vector2(-10, 0))
