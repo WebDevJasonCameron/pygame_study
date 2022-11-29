1f61312aca5314e77425aa9ef5a421b548775f86
Jason Cameron <dev.jasoncameron@gmail.com>
2022-11-28 20:08:25 -0600

    turning dict keys into a list, to manage what wpn img is to be shown

--- pygame_zelda/code/player.py
+++ pygame_zelda/code/player.py
@@ -24,9 +24,14 @@ class Player(pygame.sprite.Sprite):
         self.attacking = False
         self.attack_cooldown = 400
         self.attack_time = None
-        self.create_attack = create_attack
 
         self.obstacle_sprites = obstacle_sprites
+
+        # weapon
+        self.create_attack = create_attack
+        self.weapon_index = 0
+        self.weapon = list(weapon_data.keys())[self.weapon_index]
+        print(self.weapon)
 
     def import_player_assets(self):
         character_path = '../graphics/player/'
